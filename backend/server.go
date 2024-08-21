755d03f42a75cc85788cb567230b1476345af3c6
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-08-20 21:54:56 -0400

    Added first simple route handler

--- backend/server.go
+++ backend/server.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main(){
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello, world!")
+	})
+
+	// Start Server
 	fmt.Printf("Starting Server at Port 8080\n")
 	err := http.ListenAndServe(":8080", nil)
 	if(err != nil){
