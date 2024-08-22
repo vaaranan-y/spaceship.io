9bcf3aa9753aa53e5e3c38e463cddf7f660094ea
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-08-22 18:48:07 -0400

    Created upgraded connectino for ws, and listener

--- backend/server.go
+++ backend/server.go
@@ -4,23 +4,55 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(connection *websocket.Conn){
+	for {
+		messageType, p, err := connection.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		fmt.Println(string(p)) // Print out message
+		err = connection.WriteMessage(messageType, p)
+		if(err != nil){
+			log.Fatal(err)
+			return
+		}
+	}
+}
+
 func testEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func (r *http.Request) bool { return true } // Accept all clients for now
+
 	if(r.URL.Path != "/hello"){
 		http.Error(w, "404 Not Found", http.StatusNotFound)
 		return
 	}
 
 	if(r.Method != "GET"){
-		http.Error(w, "Unsupported Method", http.StatusNotFound)
+		http.Error(w, "Unsupported Method", http.StatusBadRequest)
 		return
 	}
 
 	fmt.Fprintf(w, "Hello, world!")
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if(err != nil){
+		http.Error(w, "Unsupported Method", http.StatusNotFound)
+	} 
+	reader(ws)
 }
 
 func main(){
+	// Set up routes
 	http.HandleFunc("/hello", testEndpoint)
 
 	// Start Server
