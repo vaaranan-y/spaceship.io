88994c9ce968aea68563f4177efd02bff5470cff
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-08-25 01:18:23 -0400

    Made major corrections to game class

--- backend/game.go
+++ backend/game.go
@@ -1,28 +1,29 @@
-package backend
+package main
 
 type Game struct {
 	Players map[int64]*Player
 }
 
-func (Game g) GetPlayers() int64 {
+func (g *Game) GetPlayers() map[int64]*Player {
 	return g.Players
 }
 
 func CreateGame() *Game {
 	return &Game{
-		Players: make(map[int64]Player)
+		Players: make(map[int64]*Player),
 	}
 }
 
-func (Game g) AddPlayer(playerToAdd *Player) bool{
+func (g *Game) AddPlayer(playerToAdd *Player) bool{
 	g.Players[playerToAdd.ID] = playerToAdd
 	return true
 }
 
-func (Game g) RemovePlayer(playerID int64) bool{
-	player, exists := g.Players[playerID]
+func (g *Game) RemovePlayer(playerID int64) bool{
+	_, exists := g.Players[playerID]
 	if(exists){
 		delete(g.Players, playerID)
+		return true
 	} else {
 		return false
 	}
