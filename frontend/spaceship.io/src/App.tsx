20e1ff208d3ec726b996e9a6f0207ce6b7cdb911
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-09-13 17:17:16 -0400

    Added frontend for application

---
+++ frontend/spaceship.io/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="TitleCard">
+      SPACESHIP.IO
+      </div>
+    </div>
+  );
+}
+
+export default App;
