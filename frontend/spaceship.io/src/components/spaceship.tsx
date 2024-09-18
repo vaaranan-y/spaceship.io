1c1fe46c1aba7ebb39f1323115c89e3c4d01c36c
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-09-17 21:52:47 -0400

    Updated spaceship component to use p5.js

--- frontend/spaceship.io/src/components/spaceship.tsx
+++ frontend/spaceship.io/src/components/spaceship.tsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
-import "./spaceship.css"
+import Sketch from 'react-p5';
+import p5Types from 'p5'; // Import this for type hinting
 
-interface SpaceshipProps {
-    x: number;
-    y: number;
-}
+export default function Soaceship() {
+  const [position, setPosition] = useState({ x: 300, y: 300 });
+  const maxSpeed = 3;
+  
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+  };
 
-function Spaceship({x, y} : SpaceshipProps) {
-    return (
-        <div className="spaceship" style={{ left: x, top: y }}>
-        </div>
-    );
-}
+  const draw = (p5: p5Types) => {
+    p5.background(100);
+    
+    const dirX = p5.mouseX - position.x;
+    const dirY = p5.mouseY - position.y;
+    const angle = Math.atan2(dirY, dirX);
+    
+    const velX = Math.cos(angle) * maxSpeed;
+    const velY = Math.sin(angle) * maxSpeed;
 
-export default Spaceship;
+    setPosition(prev => ({
+      x: prev.x + velX,
+      y: prev.y + velY,
+    }));
+
+    p5.fill(0);
+    p5.rect(position.x, position.y, 50, 50);
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+};
+
+
