15bfa8652c98c27259aea3fb44a6e70d25b44658
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-09-18 21:56:29 -0400

    Removed unnecessary code

--- frontend/spaceship.io/src/App.tsx
+++ frontend/spaceship.io/src/App.tsx
@@ -3,21 +3,6 @@ import Spaceship from './components/spaceship';
 import './App.css';
 
 function App() {
-  const [x, setX] = useState<number>(0);
-  const [y, setY] = useState<number>(0);
-
-
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      console.log(`{x: ${event.clientX}, y:${event.clientX}}`)
-      setX(event.clientX)
-      setY(event.clientY)
-    }
-    window.addEventListener('mousemove', handleMouseMove);
-  }, [])
-
-
-  
   return (
     <div className="App">
       <Spaceship/>
