5233810566f8d55d137014b48a7612ed185c1c36
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-09-19 21:00:36 -0400

    Added connection to websocket

--- frontend/spaceship.io/src/App.tsx
+++ frontend/spaceship.io/src/App.tsx
@@ -1,9 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Spaceship from './components/spaceship';
 import './App.css';
 
 export default function App() {
   const [players, setPlayers]  = useState([])
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8080/join");
+    ws.current.onmessage = (event) => {
+      const message = event.data;
+      console.log(message)
+    }
+    
+    ws.current.onopen = () => {
+      console.log('WebSocket Connection Opened')
+    }
+
+    ws.current.onerror = () => {
+      console.log('WebSocket Connection Error!')
+    }
+  }, [])
   return (
     <div className="App">
       {
