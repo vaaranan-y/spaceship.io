48ec931b1b9f4ad95ae6b1d5ca97f676c8d64303
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-09-22 19:31:00 -0400

    Cleaned up main code using abstracted code

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,33 +1,92 @@
 package main
 
-type Game struct {
-	Players map[int64]*Player
-	GameState string
-}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"spaceshipio/backend/internal/game"
+	"spaceshipio/backend/api"
+)
 
-func (g *Game) GetPlayers() map[int64]*Player {
-	return g.Players
-}
 
-func CreateGame() *Game {
-	return &Game{
-		Players: make(map[int64]*Player),
-		GameState: "waiting",
-	}
-}
+// var gameStarted bool
+// var gameStartTime time.Time
 
-func (g *Game) AddPlayer(playerToAdd *Player) bool{
-	g.Players[playerToAdd.ID] = playerToAdd
-	return true
-}
+// type PositionMessage struct {
+// 	Type string  `json:"type"`
+// 	ID   int64 `json:"id"`
+// 	X    float64 `json:"x"`
+// 	Y    float64 `json:"y"`
+// }
 
-func (g *Game) RemovePlayer(playerID int64) bool{
-	_, exists := g.Players[playerID]
-	if(exists){
-		delete(g.Players, playerID)
-		return true
-	} else {
-		return false
-	}
+// type PlayerPositions struct {
+// 	Type 	string					`json:"type"`
+// 	Players map[int64](map[string](float64))	`json:"players"`
+// }
+
+
+
+// func gameLoop(game *Game) {
+// 	tick := time.Tick(1000*time.Millisecond)
+// 	alertStart := false
+
+// 	for t := range tick {
+// 		fmt.Printf("Game Loop Update: %v\n", t)
+		
+// 		if(playerCount >= 2 && !alertStart){
+// 			alertStart = true
+// 			gameStarted = true
+// 			gameStartTime = time.Now().Add(5 * time.Second)
+// 		} else if(gameStarted && time.Now().After(gameStartTime)) {
+
+			
+// 			playerMap := PlayerPositions{
+// 				Type: "PlayerPositions",
+// 				Players: make(map[int64](map[string](float64))),
+// 			}
 	
+
+// 			for _, player := range game.Players {
+// 				coordinates := map[string]float64{
+// 					"x":player.PosX,
+// 					"y":player.PosY,
+// 				}
+// 				playerMap.Players[player.ID] = coordinates
+				
+// 			}
+
+
+// 			playerPositionsMsg := jsonifyData(playerMap)
+// 			for _, player := range game.Players {
+// 				playerConn := player.Conn
+// 				playerConn.WriteMessage(websocket.TextMessage, playerPositionsMsg)
+// 			}
+
+// 		}
+// 	}
+// }
+
+func setUpRoutes(gameManager *gameManager.GameManager){
+	// Add all routes here
+	http.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
+        api.PlayerConnectionEndpoint(gameManager, w, r)
+    })
 }
+
+func startServer(){
+	// Start the server on port 8080
+	fmt.Printf("Starting Server at Port 8080\n")
+	err := http.ListenAndServe(":8080", nil)
+	if(err != nil){
+		log.Fatal(err)
+	}
+}
+
+
+func main(){
+	gameManager := gameManager.CreateGameManager()
+	setUpRoutes(gameManager)
+	go startServer()
+	// go gameLoop(game)
+	select {}
+}	
